c772e70525d118374ce8a782cd48e26cbfd3ff13
Dilhan D <Dilhan.dincer@epitech.eu>
2026-07-02 18:19:33 +0200

    updated flask and html

--- app/app.py
+++ app/app.py
@@ -7,7 +7,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     hostname = socket.gethostname()
-    return render_template("index.html", hostname=hostname)
+    nom = os.getenv("NOM", "Visiteur")
+
+    return render_template(
+        "index.html",
+        hostname=hostname,
+        nom=nom
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
